41dd175aa428dba7aa3ce9af345774b9f0b7ce6e
Kimun Kim <kkm97351@gmail.com>
2021-09-07 03:36:54 +0900

    feat: Member와 Team의 JPA Repository 생성

---
+++ data-jpa/src/main/java/study/datajpa/repository/TeamJpaRepository.java
@@ -0,0 +1,39 @@
+package study.datajpa.repository;
+
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamJpaRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    public List<Team> findAll() {
+        return em.createQuery("SELECT t FROM Team t", Team.class)
+                .getResultList();
+    }
+
+    public Optional<Team> findById(Long id) {
+        Team team = em.find(Team.class, id);
+        return Optional.of(team);
+    }
+
+    public long count() {
+        return em.createQuery("SELECT count(t) FROM Team t", Long.class)
+                .getSingleResult();
+    }
+}
